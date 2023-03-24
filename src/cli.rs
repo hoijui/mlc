87242debdc123dc666a65c0050e1364db330357b
Robin Vobruba <hoijui.quaero@gmail.com>
2023-03-24 11:59:44 +0100

    Removes superfluous commas after `Arg`s [minor]
    # Conflicts:
    #	src/cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,7 @@ use crate::Config;
 use crate::OptionalConfig;
 use clap::Arg;
 use clap::ArgAction;
+use clap::Command;
 use std::convert::TryFrom;
 use std::fs;
 use std::path::Path;
@@ -21,54 +22,87 @@ pub fn parse_args() -> Config {
         Err(_) => OptionalConfig::default(),
     };
 
-    let matches = command!()
-        .arg(Arg::new("directory")
-            .help("Check all links in given directory and subdirectory")
-            .required(false)
-            .index(1))
-        .arg(arg!(-d --debug "Print debug information to console")
-            .required(false))
-        .arg(arg!(-o --offline "Do not check web links")
-            .alias("no-web-links")
-            .required(false))
-        .arg(Arg::new("match-file-extension")
-            .long("match-file-extension")
-            .short('e')
-            .action(ArgAction::SetTrue)
-            .help("Check the exact file extension when searching for a file")
-            .required(false))
-        .arg(Arg::new("ignore-path")
-            .long("ignore-path")
-            .short('p')
-            .help("Comma separated list of files and directories which will be ignored")
-            .value_name("PATHS")
-            .value_delimiter(',')
-            .action(ArgAction::Append)
-            .required(false))
-        .arg(Arg::new("ignore-links")
-            .long("ignore-links")
-            .short('i')
-            .value_name("LINKS")
-            .value_delimiter(',')
-            .action(ArgAction::Append)
-            .help("Comma separated list of links which will be ignored")
-            .required(false))
-        .arg(Arg::new("markup-types")
-            .long("markup-types")
-            .short('t')
-            .value_name("TYPES")
-            .help("Comma separated list of markup types which shall be checked")
-            .action(ArgAction::Append)
-            .value_delimiter(',')
-            .required(false))
-        .arg(arg!(-T --throttle <DELAY_MS> "Wait time in milliseconds between http request to the same host")
-            .required(false))
-        .arg(Arg::new("root-dir")
-            .long("root-dir")
-            .short('r')
-            .value_name("DIR")
-            .help("Path to the root folder used to resolve all relative paths")
-            .required(false))
+    let matches = Command::new(crate_name!())
+        .arg(
+            Arg::new("directory")
+                .help("Check all links in given directory and subdirectory")
+                .required(false)
+                .index(1),
+        )
+        .arg(
+            Arg::new("debug")
+                .long("debug")
+                .short('d')
+                .help("Print debug information to console")
+                .required(false),
+        )
+        .arg(
+            Arg::new("offline")
+                .long("offline")
+                .short('o')
+                .help("Do not check web links")
+                .required(false),
+        )
+        .arg(
+            Arg::new("match-file-extension")
+                .long("match-file-extension")
+                .short('e')
+                .help("Do check for the exact file extension when searching for a file")
+                .action(ArgAction::SetTrue)
+                .required(false),
+        )
+        .arg(
+            Arg::new("ignore-path")
+                .long("ignore-path")
+                .short('p')
+                .help("List of files and directories which will not be checked")
+                .value_name("PATHS")
+                .value_delimiter(',')
+                .action(ArgAction::Append)
+                .required(false),
+        )
+        .arg(
+            Arg::new("ignore-links")
+                .long("ignore-links")
+                .short('i')
+                .help("List of links which will not be checked")
+                .value_name("LINKS")
+                .value_delimiter(',')
+                .action(ArgAction::Append)
+                .required(false),
+        )
+        .arg(
+            Arg::new("markup-types")
+                .long("markup-types")
+                .short('t')
+                .value_name("TYPES")
+                .help("List of markup types which shall be checked")
+                .action(ArgAction::Append)
+                .value_delimiter(',')
+                .value_parser(["md", "html"])
+                .required(false),
+        )
+        .arg(
+            Arg::new("throttle")
+                .long("throttle")
+                .short('T')
+                .num_args(1)
+                .value_name("DELAY_MS")
+                .help("Wait between http request to the same host for a defined number of milliseconds")
+                .required(false)
+        )
+        .arg(
+            Arg::new("root-dir")
+                .long("root-dir")
+                .short('r')
+                .num_args(1)
+                .value_name("DIR")
+                .help("Path to the root folder used to resolve all relative paths")
+                .required(false)
+        )
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
         .get_matches();
 
     let default_dir = format!(".{}", &MAIN_SEPARATOR);
